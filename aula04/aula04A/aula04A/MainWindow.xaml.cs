ac352e79814e8e412f5ab3af98b275fad23d9ef5
João Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2025-09-05 21:41:46 -0300

    aula 04 05/09/2025

---
+++ aula04/aula04A/aula04A/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace aula04A
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (int.TryParse(txt_qntprimos.Text, out int qntPrimos) && qntPrimos > 0)
+                {
+                    var segundaJanela = new SegundaJanela(qntPrimos);
+                    segundaJanela.Show();
+                } else
+                {
+                    MessageBox.Show("Por favor, digite um número valido maior que zero.", "Erro", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show($"Erro: {ex.Message}", "Erro", MessageBoxButton.OK);
+            }
+        }
+    }
+}
---
+++ aula04/aula04A/aula04A/SegundaJanela.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace aula04A
+{
+    /// <summary>
+    /// Lógica interna para SegundaJanela.xaml
+    /// </summary>
+    public partial class SegundaJanela : Window
+    {
+        private List<int> numerosPrimos;
+        private int qntPrimos;
+
+        public SegundaJanela(int qntPrimos)
+        {
+            InitializeComponent();
+
+            this.qntPrimos = qntPrimos;
+            this.numerosPrimos = new List<int>();
+        }
+
+        private void btn_salvar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SalvarNoBanco();
+                MessageBox.Show("Números primos salvos com sucesso no banco de dados!", "Sucesso", MessageBoxButton.OK, MessageBoxImage.Information);
+            } catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao salvar no Banco de dados: {ex.Message}", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void SalvarNoBanco()
+        {
+            string connectionString = "Server=LAB9-28;Database=numerosPrimosDois;Integrated Security=true;TrustServerCertificate=true;";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // INSERIR NÚMEROS PRIMOS
+                foreach (int primo in numerosPrimos)
+                {
+                    string insertQuery = "INSERT INTO dboNumeros (NumeroPrimo) VALUES (@numeros)";
+
+                    using (var command = new SqlCommand(insertQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@numero", primo);
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+    }
+}
